7689eaac8fdc272a1045718cac553f786b48261a
Nour <nourelhoudanabili@gmail.com>
2023-03-19 03:46:29 -0700

    f

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /** more headers goes there */
 
 /** betty style doc for function main goes there */
-int main()
+int main(void)
 {
 	int n;
 
@@ -21,6 +21,6 @@ int main()
 	else
 	{
     printf("%d is 0",n);
-	return (0);
 }
+       return (0);
 }
