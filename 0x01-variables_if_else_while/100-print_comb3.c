374e1f0b7427eb496ff9c71db6afb6cb9a106532
Nour <nourelhoudanabili@gmail.com>
2023-03-20 19:08:24 -0700

    f

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+    int x, y;
+for (x = 0; x < 10; x++){
+for (y = 0; y < 10;y++)
+            {
+                while (y!=x);
+            putchar(x);
+            putchar(y);
+        }
+}
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    putchar("\n");
+    return 0;
+}
