a23dd0ab95b86b67767ac34d2232fdfeaa492d15
Nour <nourelhoudanabili@gmail.com>
2023-03-22 05:12:19 -0700

    f

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,21 +4,22 @@
 /*
  * print_sign : prints the sign of a given number
  */
+
 int print_sign(int n)
 {	
 if (n > 0)
 {
 _putchar('+');
-}
 return (1);
+}
 else if (n < 0)
 {
 _putchar('-');
-}
 return (-1);
+}
 else
 {
 _putchar(0);
-}
 return (0);
+}
 }
