16ebd9edeaec95491a29c3082e712bf434b097cd
Nour <nourelhoudanabili@gmail.com>
2023-03-21 19:00:09 -0700

    f

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - entry point
  * function print_alphabet_x10
@@ -8,16 +8,14 @@
 
 void print_alphabet_x10(void)
 {
-    for (int y = 0 ; y<10 ; y++){
-    char i = 'a';
-    while (i<='z'){
-    _putchar_nolock( i );
-   i++;
-    }
-    }
-_putchar_nolock('\n');
+for (int y = 0 ; y <= 10 ; y++){
+char i = 'a';
+while (i<='z')
+{
+_putchar(i);
+i++;
 }
-int main(){
-    print_alphabet_x10();
-return 0;
 }
+_putchar('\n');
+}
+return;
