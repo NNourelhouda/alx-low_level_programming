aa5773b1b58ca3f76a552c9bdaf8a42c5c9b5f5f
Nour <nourelhoudanabili@gmail.com>
2023-03-21 18:21:37 -0700

    f

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
+
 
 /* putchar function prototype */
 int _putchar(char c);
 /* print_alphabet function prototypes */
 void print_alphabet(void);
 
-#endif /* MAIN_H */
+
+#endif
