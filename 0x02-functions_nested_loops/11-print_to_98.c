668c0efe0585de8287c99a2c8a20c8310b5ffc8d
Nour <nourelhoudanabili@gmail.com>
2023-03-22 19:57:30 -0700

    f

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,19 +3,55 @@
 
 void print_to_98(int n)
 {
-if (n <= 98)
+/**
+ * print_to_98 - prints all numbers from n to 98, followed by a new line
+ * @n: starting number
+ *
+ * Return: void
+ */
+void print_to_98(int n)
 {
-_putchar('n');
-_putchar(',');
-_putchar(' ');
-n++;
-}
-else if (n > 98)
-{
-_putchar('n');
-_putchar(',');
-_putchar(' ');
-n--;
-}
-
+    if (n <= 98)
+    {
+        for (int i = n; i <= 98; i++)
+        {
+            if (i < 0)
+            {
+                putchar('-');
+                i = abs(i);
+            }
+            if (i / 10 == 0)
+            {
+                putchar('0');
+            }
+            printf("%d", i);
+            if (i != 98)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    else
+    {
+        for (int i = n; i >= 98; i--)
+        {
+            if (i < 0)
+            {
+                putchar('-');
+                i = abs(i)
+			  }
+            if (i / 10 == 0)
+            {
+                putchar('0');
+            }
+            printf("%d", i);
+            if (i != 98)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    putchar('\n');
 }
