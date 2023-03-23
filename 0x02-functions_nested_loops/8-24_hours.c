3337626e162e269a68f77f62a12c93f36c116efa
Nour <nourelhoudanabili@gmail.com>
2023-03-22 17:56:41 -0700

    f

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@
 void jack_bauer(void) 
 {
 int hour, minute;
-for (hour = 0;hour < 24;hour++) 
+for (hour = 0; hour < 24; hour++)
 {
-for (minute = 0;minute < 60;minute++) 
+for (minute = 0; minute < 60; minute++)
 {
 putchar(hour / 10 + '0');
 putchar(hour % 10 + '0');
