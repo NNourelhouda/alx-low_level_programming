3c7c4b3a5b1a19b4aa766784a34b189de89a06a9
Nour <nourelhoudanabili@gmail.com>
2023-03-24 15:34:53 -0700

    f

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,9 +10,10 @@
 * Return: void
 */
 
-void print_remaining_days(int day, int year)
+void print_remaining_days(int day,int month, int year)
 {
 if  (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
+{
 if (month >= 2)
 {
 day++;
