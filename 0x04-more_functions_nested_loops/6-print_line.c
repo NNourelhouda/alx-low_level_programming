75731674b21d49c0882c1599a3bb3f4e2b74b1c8
Nour <nourelhoudanabili@gmail.com>
2023-03-24 15:09:06 -0700

    f

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,14 +12,14 @@ void print_line(int n)
 {
 if (n <= 0)
 {
-_putchar('\n');
+putchar('\n');
 }
 else
 {
 for (int i = 1; i < n; i++)
 {
-_putchar('_');
+putchar('_');
 }
-_putchar('\n');
+putchar('\n');
 }
 }
