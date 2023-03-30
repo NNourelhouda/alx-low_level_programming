15aab9c9d579998d6da4ff049a80e4c928afadbf
Nour <nourelhoudanabili@gmail.com>
2023-03-30 04:23:57 -0700

    f

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,16 +12,13 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 1; i < n ; i++)
+for (i = 0; i < n ; i++)
 {
 printf ("%d",*(a+i));
-if (i<n-1)
+if (i != n - 1) 
 {
-printf (", ");
+printf(", ");
 }
-else 
-{
+}
 printf ("\n");
-}
-}
 }
