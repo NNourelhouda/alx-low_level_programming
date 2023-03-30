4720eb416cb0e5573718ef5ca1939f8448b83b93
Nour <nourelhoudanabili@gmail.com>
2023-03-30 07:10:06 -0700

    f

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    int i;
+    char password[100];
+
+    srand(time(NULL));
+
+    for (i = 0; i < 100; i++)
+    {
+        password[i] = rand() % 94 + 33;
+    }
+
+    printf("%s\n", password);
+
+    return (0);
+}
