b7b06204412192ebff656dedea14151b299e16b7
Nour <nourelhoudanabili@gmail.com>
2023-04-07 12:02:10 -0700

    f

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,47 +2,55 @@
 #include "main.h"
 #include <math.h>
 /**
- * _pow_recursion - returns the natural square root of a number
- * @n: number
+ * _sqrt_recursion - Calculates the natural square root of a number
+ * @n: The number to find the square root of
  *
- * Return: The natural square root
+ * Return: The square root of n if it is a perfect square, -1 otherwise
  */
-int _sqrt_helper(int n, int low, int high);
-
-int _sqrt_recursion(int n) {
-    if (n < 0) {
-        /* error: negative number */
-        return -1;
-    } else if (n == 0) {
-        /* base case: square root of 0 is 0 */
-        return 0;
-    } else {
-        /* start recursive search */
-        return _sqrt_helper(n, 1, n);
-    }
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
 }
-
-int _sqrt_helper(int n, int low, int high) {
-    int mid;
-    int sqr;
-
-    if (high < low) {
-        /* error: no natural square root */
-        return -1;
-    }
-
-    mid = (low + high) / 2; /* integer division, rounds down */
-    sqr = mid * mid;
-
-    if (sqr == n) {
-        /* found natural square root */
-        return mid;
-    } else if (sqr > n) {
-        /* search left half */
-        return _sqrt_helper(n, low, mid - 1);
-    } else {
-        /* search right half */
-        return _sqrt_helper(n, mid + 1, high);
-    }
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (_sqrt_helper(n, 1, n));
+}
+}
+/**
+ * _sqrt_helper - Recursive helper function for _sqrt_recursion
+ * @n: The number to find the square root of
+ * @low: The lowest possible value for the square root
+ * @high: The highest possible value for the square root
+ *
+ * Return: The square root of n if it is a perfect square, -1 otherwise
+ */
+int _sqrt_helper(int n, int low, int high)
+{
+int mid;
+int sqr;
+if (high < low)
+{
+return (-1);
+}
+mid = (low + high) / 2;
+sqr = mid * mid;
+if (sqr == n)
+{
+return (mid);
+}
+else if (sqr > n)
+{
+return (_sqrt_helper(n, low, mid - 1));
+}
+else
+{
+return (_sqrt_helper(n, mid + 1, high));
+}
 }
 
