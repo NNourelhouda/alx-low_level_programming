8784e88118d398cfe61800483580fd6bdb4351f5
Nour <nourelhoudanabili@gmail.com>
2023-04-07 11:49:27 -0700

    f

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,39 +7,42 @@
  *
  * Return: The natural square root
  */
-int _sqrt_recursion(int n)
-{
-if (n < 0)
-{
-return (-1);
+int _sqrt_helper(int n, int low, int high);
+
+int _sqrt_recursion(int n) {
+    if (n < 0) {
+        /* error: negative number */
+        return -1;
+    } else if (n == 0) {
+        /* base case: square root of 0 is 0 */
+        return 0;
+    } else {
+        /* start recursive search */
+        return _sqrt_helper(n, 1, n);
+    }
 }
-else if (n == 0 || n == 1)
-{
-return (n);
+
+int _sqrt_helper(int n, int low, int high) {
+    int mid;
+    int sqr;
+
+    if (high < low) {
+        /* error: no natural square root */
+        return -1;
+    }
+
+    mid = (low + high) / 2; /* integer division, rounds down */
+    sqr = mid * mid;
+
+    if (sqr == n) {
+        /* found natural square root */
+        return mid;
+    } else if (sqr > n) {
+        /* search left half */
+        return _sqrt_helper(n, low, mid - 1);
+    } else {
+        /* search right half */
+        return _sqrt_helper(n, mid + 1, high);
+    }
 }
-else
-{
-return (_sqrt_helper(n, 1, n));
-}
-}
-int _sqrt_helper(int n, int start, int end)
-{
-if (start > end)
-{
-return (-1);
-}
-int mid;
-mid = (start + end) / 2;
-if (mid * mid == n)
-{
-return (mid);
-}
-else if (mid * mid > n)
-{
-return (_sqrt_helper(n, start, mid - 1));
-}
-else
-{
-return (_sqrt_helper(n, mid + 1, end));
-}
-}
+
